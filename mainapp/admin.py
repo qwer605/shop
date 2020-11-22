5bc26c7d37e2d7fe3c2e490a5416f410272dc087
qwer605 <tolockoyegor@gmail.com>
2020-11-22 17:05:41 +0200

    basic features

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
+from django.forms import ModelChoiceField, ModelForm
+
 
 # Register your models here.
+
+
+admin.site.register(Category)
+admin.site.register(Notebook)
+admin.site.register(Smartphone)
+admin.site.register(Cart)
+admin.site.register(Customer)
+admin.site.register(CartProduct)
